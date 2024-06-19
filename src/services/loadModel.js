7b6de20c39227224fcbe1d5aa5b709d8a2183b42
Clumsy03 <adityasagara003@gmail.com>
2024-06-19 22:31:06 +0700

    Delete src directory

--- src/services/loadModel.js
+++
@@ -1,8 +0,0 @@
-const tf = require('@tensorflow/tfjs-node');
- 
-async function loadModel() {
-    const model = await tf.loadGraphModel('https://storage.googleapis.com/capstone_mytrash/saved/saved/model.json');
-    return model
-}
- 
-module.exports = loadModel;
